83c04f4c1426235e15dbb17b1743aab3f590e670
porpyry <porpyry@gmail.com>
2023-11-29 02:28:39 +0900

    Added custom MutationObserver classes

--- js/common/class-variables.js
+++ js/common/class-variables.js
@@ -7,47 +7,13 @@ const APP_UI_BUTTON_CLASS = APP_NAME + '__pzp-pc-ui-button';
 const APP_QUALITY_DISPLAY_CLASS = APP_NAME + '__quality-display';
 const APP_PLAYBACK_RATE_DISPLAY_CLASS = APP_NAME + '__playback-rate-display';
 
-// prism player own classes
-const PRISM_PLAYER_CLASS = 'pzp-pc';
-const PLAYER_QUALITY_SETTING_PANE_VISIBLE_CLASS = 'pzp-pc--setting-quality';
-const PLAYER_PLAYBACK_RATE_SETTING_PANE_VISIBLE_CLASS = 'pzp-pc--setting-playbackrate';
-const PLAYER_VOLUME_CONTROL_ACTIVE_CLASS = 'pzp-pc--active-volume-control';
-const VIDEO_BEFORE_PLAY_CLASS = 'pzp-pc--beforeplay';
-const VIDEO_PLAYING_CLASS = 'pzp-pc--playing';
-const VIDEO_ENDED_CLASS = 'pzp-pc--ended';
-const VIDEO_LOADING_CLASS = 'pzp-pc--loading';
-
-// prism player own elements
-const PLAYER_DIM_CLASS = 'pzp-pc__dimmed';
-const PLAYER_HEADER_CLASS = 'pzp-pc__header';
-const PLAYER_PLAY_BUTTON_CLASS = 'pzp-pc__brand-playback-button'; // center play button
-const PLAYER_BOTTOM_CLASS = 'pzp-pc__bottom';
-const PLAYER_PLAY_PAUSE_BUTTON_CLASS = 'pzp-pc__playback-switch'; // bottom-left play button
-const PLAYER_VOLUME_BUTTON_CLASS = 'pzp-pc__volume-button';
-const PLAYER_VOLUME_SLIDER_CLASS = 'pzp-pc__volume-slider';
-const PLAYER_BOTTOM_RIGHT_BUTTONS_CLASS = 'pzp-pc__bottom-buttons-right';
-const PLAYER_SETTING_BUTTON_CLASS = 'pzp-pc__setting-button';
-const PLAYER_FULL_SCREEN_BUTTON_CLASS = 'pzp-pc__fullscreen-button';
+// prism player related classes
+const CHECKED_SETTING_ITEM_CLASS = 'pzp-pc-ui-setting-item--checked';
+const QUALITY_SETTING_ITEM_SPAN_CLASS = 'pzp-pc-ui-setting-quality-item__prefix';
+const PLAYBACK_RATE_SETTING_ITEM_SPAN_CLASS = 'pzp-pc-ui-setting-playbackrate-item__value';
 
 // prism player style classes
 const PLAYER_BUTTON_CLASS = 'pzp-button';
 const PLAYER_UI_BUTTON_CLASS = 'pzp-pc-ui-button';
-
-// prism player quality settings
-const QUALITY_SETTING_MENU_CLASS = 'pzp-pc-setting-intro-quality';
-const QUALITY_SETTING_LIST_CLASS = 'pzp-pc-setting-quality-pane__list-container';
-const QUALITY_SETTING_ITEM_CLASS = 'pzp-pc-ui-setting-quality-item';
-const QUALITY_SETTING_ITEM_TEXT_CLASS = 'pzp-pc-ui-setting-quality-item__prefix';
-
-// prism player playback rate settings
-const PLAYBACK_RATE_SETTING_MENU_CLASS = 'pzp-pc-setting-intro-playbackrate';
-const PLAYBACK_RATE_SETTING_LIST_CLASS = 'pzp-pc-setting-playbackrate-pane__list';
-const PLAYBACK_RATE_SETTING_ITEM_CLASS = 'pzp-pc-ui-setting-playbackrate-item';
-const PLAYBACK_RATE_SETTING_ITEM_TEXT_CLASS = 'pzp-pc-ui-setting-playbackrate-item__value';
-
-// prism player setting item own class
-const CHECKED_SETTING_ITEM_CLASS = 'pzp-pc-ui-setting-item--checked';
-
-// prism player button tooltip classes
 const PLAYER_UI_TOOLTIP_CLASS = 'pzp-pc-ui-button__tooltip';
 const PLAYER_UI_TOOLTIP_TOP_CLASS = 'pzp-pc-ui-button__tooltip--top';
--- js/common/util.js
+++ js/common/util.js
@@ -46,4 +46,49 @@ function getOrObserveChildByClassName(parent, className, callback) {
         }
     };
 }
+
+class ClassChangeObserver extends MutationObserver {
+
+    static options = { attributeOldValue: true, attributeFilter: ['class'] };
+
+    // callback: (appeared: boolean, target: Element, observer) => void
+    constructor(className, callback) {
+        super((mutationList, observer) => {
+            for (const mutation of mutationList) {
+                const existed = mutation.oldValue.includes(className);
+                const exist = mutation.target.classList.contains(className);
+                if (existed != exist) {
+                    callback(exist, mutation.target, observer);
+                }
+            }
+        });
+    }
+
+    observe(target) {
+        super.observe(target, ClassChangeObserver.options);
+    }
+}
+
+class TextChangeObserver extends MutationObserver {
+
+    static options = { characterData: true };
+
+    // callback: (changedText: string, target: Element) => void
+    constructor(callback) {
+        super((mutationList) => {
+            for (const mutation of mutationList) {
+                const textNode = mutation.target;
+                callback(textNode.textContent, textNode.parentElement);
+            }
+        });
+    }
+
+    // target: Element whose first child is TEXT_NODE
+    observe(target) {
+        const textNode = target.firstChild;
+        if (textNode?.nodeType !== Node.TEXT_NODE) {
+            return;
+        }
+        super.observe(textNode, TextChangeObserver.options);
+    }
 }
