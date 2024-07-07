90265de475858318c5d80876e29c268ae272fd3a
porpyry <porpyry@gmail.com>
2024-07-07 14:58:06 +0900

    Changed feature Easy-Open-Volume-Slider

--- js/decorator/easy-open-volume-slider-decorator.js
+++ js/decorator/easy-open-volume-slider-decorator.js
@@ -1,63 +1,13 @@
 class EasyOpenVolumeSliderDecorator extends Decorator {
 
     decorate(prismPlayer) {
-        const volumeControl = prismPlayer.query('volumeControl');
-        volumeControl.prepend(this.createVolumeSliderHoverHelper());
-
-        const bottomRightButtons = prismPlayer.query('bottomRightButtons');
-        const blocker = this.createVolumeSliderHoverHelperBlocker();
-        bottomRightButtons.prepend(blocker);
-
-        // observe width of bottomRightButtons
-        const resizeObserver = new ResizeObserver((entries) => {
-            for (const entry of entries) {
-                const blocker = this.getVolumeSliderHoverHelperBlocker(prismPlayer);
-                this.setBlockerWidth(blocker, entry.target.clientWidth);
-            }
-        });
-        resizeObserver.observe(bottomRightButtons);
-
-        // apply now
-        this.setBlockerWidth(blocker, bottomRightButtons.clientWidth);
-
-        prismPlayer.attachListeners(this, { resizeObserver });
+        const bottom = prismPlayer.query('bottom');
+        bottom.classList.add(APP_EASY_OPEN_VOLUME_SLIDER_CLASS);
     }
 
     async clear(prismPlayer) {
-        const { resizeObserver } = prismPlayer.getAttachedListeners(this);
-
-        this.getVolumeSliderHoverHelper(prismPlayer).remove();
-        this.getVolumeSliderHoverHelperBlocker(prismPlayer).remove();
-
-        resizeObserver.disconnect();
-
-        prismPlayer.detachListeners(this);
+        const bottom = prismPlayer.query('bottom');
+        bottom.classList.remove(APP_EASY_OPEN_VOLUME_SLIDER_CLASS);
         return true;
-    }
-
-    getVolumeSliderHoverHelper(prismPlayer) {
-        return prismPlayer.element.querySelector('.' + APP_VOLUME_SLIDER_HOVER_HELPER_CLASS);
-    }
-
-    createVolumeSliderHoverHelper() {
-        const div = document.createElement('div');
-        div.classList.add(APP_VOLUME_SLIDER_HOVER_HELPER_CLASS);
-        return div;
-    }
-
-    getVolumeSliderHoverHelperBlocker(prismPlayer) {
-        return prismPlayer.element.querySelector('.' + APP_VOLUME_SLIDER_HOVER_HELPER_BLOCKER_CLASS);
-    }
-
-    createVolumeSliderHoverHelperBlocker() {
-        const div = document.createElement('div');
-        div.classList.add(APP_VOLUME_SLIDER_HOVER_HELPER_BLOCKER_CLASS);
-        return div;
-    }
-
-    setBlockerWidth(blocker, controlWidth) {
-        // margin-right: 23px
-        // margin-left: 10px
-        blocker.style.width = (controlWidth + 23 - 10).toFixed() + 'px';
     }
 }
